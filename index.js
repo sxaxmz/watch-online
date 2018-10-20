ac497460559c093aa7a39850b28c90987ae6d3a7
Osama Hussein <osamathadon@gmail.com>
2018-10-20 23:11:59 +0800

    Notification-bar animation and dark mode adaption

--- index.js
+++ index.js
@@ -54,16 +54,25 @@ card.forEach( function(el) {
 });
 
 var cardContainer = document.getElementById("card-group-container");
+var card = document.getElementsByClassName("card");
+var notificationBar = document.getElementById("Notification");
+var notifyTxt = document.getElementById("notifyTxt");
+
 function darkMode (x) {
 	if (navbar.classList.contains("bg-light")) {
 		navbar.classList.add("bg-dark");
-		navbar.classList.remove("bg-light");
 		document.body.style.backgroundColor = "#5C6369";
 		cardContainer.style.borderColor = "#28A745";
+		notifyTxt.style.color = "#28A745";
+		notificationBar.style.backgroundColor = "#5C6369";
+		navbar.classList.remove("bg-light");
 	} else {
 		navbar.classList.add("bg-light");
-		navbar.classList.remove("bg-dark");
+		cardContainer.style.borderColor = "#000";
 		document.body.style.backgroundColor = "#fff";
+		notificationBar.style.backgroundColor = "#F8F9FA";
+		notifyTxt.style.color = "#000";
+		navbar.classList.remove("bg-dark");
 	}
 	
 }
